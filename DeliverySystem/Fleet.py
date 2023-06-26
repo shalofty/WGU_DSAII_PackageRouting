52d4e5137b04c81df39d67352d89e11a2885b881
Stephan <stephanhaloftis@gmail.com>
2023-06-26 00:45:10 -0400

    Algo is working. All packages are being delivered. Although mileage is higher than ideal, currently sitting at 204.3 with a goal of below 140.

--- DeliverySystem/Fleet.py
+++ DeliverySystem/Fleet.py
@@ -11,14 +11,17 @@ class Fleet:
     def remove(self, truck):
         self.trucks.remove(truck)
 
+
 # Create truck objects populated with specific packages
+# Each truck has specific packages to deliver
+# Refer to package notes for delivery details
 hub = "4001 South 700 East"
 truck1 = Truck(16, 18, None, [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40], 0.0, hub)
-truck2 = Truck(16, 18, None, [3, 6, 12, 17, 18, 19, 20, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0, hub)
-truck3 = Truck(16, 18, None, [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0, hub)
+truck2 = Truck(16, 18, None, [3, 6, 12, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0, hub)
+truck3 = Truck(16, 18, None, [2, 4, 5, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0, hub)
 
 # Create fleet and add truck objects to it
 fleet = Fleet()
 fleet.add(truck1)
-fleet.add(truck2)
-fleet.add(truck3)
+# fleet.add(truck2)
+# fleet.add(truck3)
--- DeliverySystem/Main.py
+++ DeliverySystem/Main.py
@@ -44,40 +44,112 @@ def buildTable(capacity):
         print("Error building table")
 
 
-def deliverPackages(truck):
-    preshipment = []
-    try:
-        for packageID in truck.packages:
-            package = hash_table.search(packageID)
-            preshipment.append(package)
-        truck.packages.clear()
-        currentAddress = Utils.getAddress(truck.address)
+# def deliverPackages(truck):
+#     preshipment = []
+#     try:
+#         for packageID in truck.packages:
+#             package = hash_table.search(packageID)
+#             preshipment.append(package)
+#         truck.packages.clear()
+#         currentaddress = Utils.getAddress(truck.address)
+#
+#         while preshipment:
+#             shortestDistance = float('inf')
+#             nextPackage = None
+#
+#             for package in preshipment:
+#                 packageaddress = Utils.getAddress(package.address)
+#                 distance = Utils.findDistance(currentaddress, packageaddress)
+#
+#                 if distance <= shortestDistance:
+#                     shortestDistance = distance
+#                     nextPackage = package
+#
+#             truck.packages.append(nextPackage.ID)
+#             preshipment.remove(nextPackage)
+#             truck.mileage += shortestDistance
+#             truck.address = nextPackage.address
+#
+#     except csv.Error as e:
+#         print("Error delivering packages")
 
-        while preshipment:
-            shortestDistance = float('inf')
-            nextPackage = None
 
-            for package in preshipment:
-                packageAddress = Utils.getAddress(package.address)
-                distance = Utils.findDistance(currentAddress, packageAddress)
+# def deliverLoad(truck):
+#     load = []
+#     distances = []
+#     try:
+#         for packageid in truck.packages:
+#             package = hash_table.search(packageid)
+#             distance = Utils.findDistance(truck.address, package.address)
+#             load.append(package)
+#             distances.append(distance)
+#             hash_table.hash_remove(packageid)
+#             nearest = min(distances)
+#             index = distances.index(nearest)
+#             # nearestaddress = addresses[index]
+#             print("Number of packages in truck: ", len(truck.packages))
+#             if len(truck.packages) == 0:
+#                 truck.mileage += sum(distances)
+#                 print(truck.mileage)
+#                 break
+#
+#         truck.packages.clear()
+#     except csv.Error as e:
+#         print("Error delivering packages")
 
-                if distance <= shortestDistance:
-                    shortestDistance = distance
-                    nextPackage = package
 
-            truck.packages.append(nextPackage.ID)
-            preshipment.remove(nextPackage)
-            truck.mileage += shortestDistance
-            truck.address = nextPackage.address
+# Currently incorporating a greedy algorithm to deliver packages
+# The algo always chooses the nearest package to the current address
+def deliver(truck):
+    packages = []
+    distances = []
+    path = []
+    outfordelivery = True
+    index = 0
+    unprocessed = True
+    currentaddress = truck.address
+    while outfordelivery:
+        if unprocessed:
+            # Assign package to variable
+            package = hash_table.search(truck.packages[index])
+            # Calculate distance from current address to package address
+            distance = Utils.findDistance(currentaddress, package.address)
+            # Add package to list of packages
+            packages.append(package)
+            # Add distance to list of distances
+            distances.append(distance)
+            # Update truck location to package address which prepares for next iteration
+            truck.updateLocation(package.address)
+            # Remove package from hash table
+            hash_table.hash_remove(package.ID)
+            # Increment delivery index
+            index += 1
+        # If incremented through all packages in truck
+        if index == (len(truck.packages)):
+            unprocessed = False  # Disables first if statement
+            # Find nearest package
+            nearest = min(distances)
+            # Find index of nearest package
+            nindex = distances.index(nearest)
+            # Add nearest package to path
+            path.append(distances[nindex])
+            # Remove nearest package from packages list
+            distances.pop(nindex)
+            truck.mileage += nearest
+        if len(distances) == 0:
+            outfordelivery = False
+            print("Truck Mileage: ", truck.mileage)
+            return truck.mileage
 
-    except csv.Error as e:
-        print("Error delivering packages")
 
 hash_table = buildTable(40)
-deliverPackages(truck1)
-deliverPackages(truck2)
-print(truck1.packages)
-print(truck2.packages)
+
+deliver(truck1)
+deliver(truck2)
+deliver(truck3)
+totalmileage = truck1.mileage + truck2.mileage + truck3.mileage
+print("Total Mileage: ", totalmileage)
+# Working on getting it below 140 miles, currently at 204.3 miles
 
 # Test Code
 # hash_table = buildTable(40)
